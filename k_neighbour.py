018053f82eb22601653772b6abdbbfabd35c2706
suzunedp <i0ho8ku0ij1nja_1029@me.com>
2017-10-17 14:18:42 +0900

    完成

--- k_neighbour.py
+++ k_neighbour.py
@@ -1,4 +1,3 @@
-from operator import itemgetter, attrgetter
 #定義した関数
 
 #距離を計算
@@ -8,18 +7,18 @@ def culculate_dist(test,teacher):
    return result
 
 #一つ抜き法用の教師データの作成
-def create_teacher_data(number,data):
-    del data[number]
-    return data
+# def create_teacher_data(number,data):
+#     del data[number]
+#     return data
 
 #k番目までの配列に入っている花の種類を数える
 def count_frequent_spices(k,result):
-    count = 0
+    count = 1
     spices_count = [0,0,0]
     while count < k:
-        if result[k][1] == 0.0:
+        if result[count][1] == 0.0:
             spices_count[0] += 1
-        elif result[k][1] == 1.0:
+        elif result[count][1] == 1.0:
             spices_count[1] += 1
         else:
             spices_count[2] += 1
@@ -36,6 +35,7 @@ def data_check(spices_list,answer):
 
 #ここからメイン
 
+#csvの読み込み------------------------------------------------------------
 import csv
 list_row = []
 raw_data = [] #元のデータを格納
@@ -48,38 +48,63 @@ with open("Iris.csv","r") as f:
         list_row = list(map_row)
         raw_data.append(list_row)
 
-#k近傍法のkの初期値
-k = 3
+#csvの読み込みここまで------------------------------------------------------
 
-#テストデータの番号
-test_number = 0
+total_result = [] #最終結果が吐かれる配列
 
-while test_number < len(raw_data):
+k = 2 #k近傍法のkの初期値
+result = []  #距離の計算結果
+sorted_result = [] #ソート済み距離の計算結果
+count_spices = [] #品種
+# teacher_number = 0
 
-    #教師用データとテスト用データに分割
-    tester = raw_data[test_number] #テストデータ
-    test_answer = tester[4] #テストデータの品種
-    teacher = create_teacher_data(test_number,raw_data) #教師用データ
+while k <= 31:
 
-    result = []  #距離の計算結果格納用配列
-    teacher_number = 0 #教師データの番号
+    k_and_success_rate = [k,0] #各kの時に何回成功したか
+    test_data_number = 0 #何番目のデータをテスト用データとして使うか
 
-    while teacher_number < len(teacher):
-        #ここでそれぞれの教師用データとテストデータの距離を計算する
-        each_result = culculate_dist(tester,teacher[teacher_number])
-        result.append(each_result)
-        teacher_number += 1
+    while test_data_number < len(raw_data):
 
-    #result配列を距離に応じて並び替える
-    sorted(result, key=itemgetter(0), reverse = True)
+        #教師用データとテスト用データに分割
+        tester = raw_data[test_data_number] #テストデータ
+        test_answer = tester[4] #テストデータの品種
 
-    #k番目までの配列に入っている花の種類を数える
-    count_spices = []
-    count_spices = count_frequent_spices(k,result)
+        teacher = raw_data #教師用データ
+        # teacher = create_teacher_data(test_data_number,raw_data) #教師用データ
 
-    #テストデータとあってるか確認
-    print(data_check(count_spices,test_answer))
+        teacher_number = 0
 
-    if test_number == 0:
+        while teacher_number < len(teacher):
+            #ここでそれぞれの教師用データとテストデータの距離を計算する
+            each_result = culculate_dist(tester,teacher[teacher_number])
+            result.append(each_result)
+            teacher_number += 1
+
+        #result配列を距離に応じて並び替える
+        sorted_result = sorted(result)
+        #k番目までの配列に入っている花の種類を数える
+
+        count_spices = count_frequent_spices(k,sorted_result)
+
+         #テストデータとあってるか確認
+        if data_check(count_spices,test_answer) == True:
+            k_and_success_rate[1] += 1
+
+        # if test_data_number == 0:
+        #     break
+
+        result.clear()
+        test_data_number += 1
+
+
+
+    total_result.append(k_and_success_rate)
+
+    if k == 1:
         break
-    test_number += 1
+
+    k += 1
+
+with open("total_result.csv", "w")as fout:
+    csvout = csv.writer(fout)
+    csvout.writerows(total_result)
